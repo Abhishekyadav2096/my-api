d6aacc533782cbb6b3e5c9771d1a5d8e66a854b8
Abhishekyadav2096 <104411173+Abhishekyadav2096@users.noreply.github.com>
2023-07-27 16:49:20 +0530

    Update app.js
    new api

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get("/", (req, res) => {
   return res.send("ABHISHEK YADAV");
 });
 
+app.get("/home", (req, res) => {
+  return res.send("CI/CD Pipeline");
+});
+
 app.listen(4002, () => {
   console.log(`Server Started at ${4002}`);
 });
